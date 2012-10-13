84174d582afafe4d752a3ee5ba952bad9edbf7ba
Jeremy Jameson <jjameson@technologytoolbox.com>
2012-10-13 16:42:31 +0000

    Avoid error when hosts file does not exist
    Workitems:
    #1413 
    git-tfs-id: [https://tfs.technologytoolbox.com/DefaultCollection]$/Toolbox/Main/PowerShell;C7295

--- Add-Hostnames.ps1
+++ Add-Hostnames.ps1
@@ -131,19 +131,28 @@ begin
 
     function ParseHostsFile
     {
-        [string] $hostsFile = $env:WINDIR + "\System32\drivers\etc\hosts"
-
-        [string[]] $hostsContent = Get-Content $hostsFile
-
         $hostsEntries = New-Object System.Collections.ArrayList
 
-        $hostsContent | foreach {
-            $hostsEntry = ParseHostsEntry $_
+        [string] $hostsFile = $env:WINDIR + "\System32\drivers\etc\hosts"
 
-            $hostsEntries.Add($hostsEntry) | Out-Null
+        If ((Test-Path $hostsFile) -eq $false)
+        {
+            Write-Verbose "Hosts file does not exist."
+        }
+        Else
+        {
+            [string[]] $hostsContent = Get-Content $hostsFile
+
+            $hostsContent | foreach {
+                $hostsEntry = ParseHostsEntry $_
+
+                $hostsEntries.Add($hostsEntry) | Out-Null
+            }
         }
 
-        return $hostsEntries
+        # HACK: Return an array (containing the ArrayList) to avoid issue with
+        # PowerShell returning $null (when hosts file does not exist)
+        return ,$hostsEntries
     }
 
     function UpdateHostsFile(
