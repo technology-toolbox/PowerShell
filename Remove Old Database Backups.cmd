5194b11f5416e1ba3bff3fd8324c51016e42f569
Jeremy Jameson <jjameson@technologytoolbox.com>
2015-10-03 12:27:03 +0000

    Allow the "NumberOfDaysToKeep" parameter to be configured in Task Scheduler
    Workitems:
    #1103 
    git-tfs-force: N/A
    git-tfs-id: [https://tfs.technologytoolbox.com/DefaultCollection]$/Toolbox/Main/PowerShell;C16952

--- Remove Old Database Backups.cmd
+++ Remove Old Database Backups.cmd
@@ -3,8 +3,10 @@
 setlocal
 
 set BACKUP_FOLDER=Z:\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Backup
+set DAYS_TO_KEEP=14
 
 if NOT "%~1" == "" set BACKUP_FOLDER=%~1
+if NOT "%~2" == "" set DAYS_TO_KEEP=%~2
 
-PowerShell.exe -Command "& 'C:\NotBackedUp\Public\Toolbox\PowerShell\Remove-OldBackups.ps1' -Path '%BACKUP_FOLDER%'; Exit $LASTEXITCODE" >> "Remove Old Database Backups.log" 2>&1
+PowerShell.exe -Command "& 'C:\NotBackedUp\Public\Toolbox\PowerShell\Remove-OldBackups.ps1' -Path '%BACKUP_FOLDER%' -NumberOfDaysToKeep %DAYS_TO_KEEP%; Exit $LASTEXITCODE" >> "Remove Old Database Backups.log" 2>&1
 EXIT %ERRORLEVEL%
