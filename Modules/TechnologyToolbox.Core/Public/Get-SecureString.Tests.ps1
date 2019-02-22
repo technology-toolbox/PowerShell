4fcaa5e6dcc1827e9eec03d6b219b58c2035e713
Jeremy Jameson <jjameson@technologytoolbox.com>
2019-02-22 16:50:19 -0700

    Add unit tests for Get-SecureString
    To run the unit tests:
    
      Invoke-Pester .\Modules\TechnologyToolbox.Core

---
+++ Modules/TechnologyToolbox.Core/Public/Get-SecureString.Tests.ps1
@@ -0,0 +1,50 @@
+. $PSScriptRoot\Get-SecureString.ps1
+
+Describe 'Get-SecureString Tests' {
+    Context '[Default prompt]' {
+        Mock Read-Host {return $securePassword}
+
+        It 'Returns expected SecureString' {
+            [System.Security.SecureString] $securePassword = `
+                ConvertTo-SecureString -String '{password}' -AsPlainText -Force
+
+            [System.Security.SecureString] $result = Get-SecureString
+
+            $result | Should Be $securePassword
+        }
+
+        Assert-MockCalled Read-Host -Times 2 -Exactly
+        Assert-MockCalled Read-Host -Times 2 -Exactly `
+            -ParameterFilter { $AsSecureString -eq $true }
+
+        Assert-MockCalled Read-Host -Times 1 -Exactly `
+            -ParameterFilter { $Prompt -eq 'Secure string' }
+
+        Assert-MockCalled Read-Host -Times 1 -Exactly `
+            -ParameterFilter { $Prompt -eq 'Secure string (confirm)' }
+    }
+
+    Context '[Custom prompt]' {
+        Mock Read-Host {return $securePassword}
+
+        It 'Uses custom prompt and returns expected SecureString' {
+            [System.Security.SecureString] $securePassword = `
+                ConvertTo-SecureString -String '{password}' -AsPlainText -Force
+
+            [System.Security.SecureString] $result = `
+                Get-SecureString -Prompt 'Password'
+
+            $result | Should Be $securePassword
+        }
+
+        Assert-MockCalled Read-Host -Times 2 -Exactly
+        Assert-MockCalled Read-Host -Times 2 -Exactly `
+            -ParameterFilter { $AsSecureString -eq $true }
+
+        Assert-MockCalled Read-Host -Times 1 -Exactly `
+            -ParameterFilter { $Prompt -eq 'Password' }
+
+        Assert-MockCalled Read-Host -Times 1 -Exactly `
+            -ParameterFilter { $Prompt -eq 'Password (confirm)' }
+    }
+}
