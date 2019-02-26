1c493b05e21630ee0d32c1cad326e2899e856a1e
Jeremy Jameson <jjameson@technologytoolbox.com>
2019-02-26 05:46:57 -0700

    Move scripts to TechnologyToolbox.Core module

---
+++ Modules/TechnologyToolbox.Core/Public/Get-Stopwatch.ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding()]
+Param()
+
+Begin
+{
+    Set-StrictMode -Version Latest
+    $ErrorActionPreference = "Stop"
+}
+
+Process
+{
+    return [System.Diagnostics.Stopwatch]::StartNew()
+}
