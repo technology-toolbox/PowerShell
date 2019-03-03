80c88c058a5c37f8dbae6768d8d51c7d372314dd
Jeremy Jameson <jjameson@technologytoolbox.com>
2019-03-03 08:29:28 -0700

    Change unit tests for RemoveRegistryKeyIfEmpty to integration tests
    - Use TestRegistry to create temporary registry items
    - Greatly simplifies tests by removing numerous mock
     functions to fake various registry items

--- Modules/TechnologyToolbox.Core/Private/RemoveRegistryKeyIfEmpty.Tests.ps1
+++ Modules/TechnologyToolbox.Core/Private/RemoveRegistryKeyIfEmpty.Tests.ps1
@@ -1,105 +1,36 @@
 . $PSScriptRoot\RemoveRegistryKeyIfEmpty.ps1
 
 Describe 'RemoveRegistryKeyIfEmpty Tests' {
-    Mock Get-ChildItem {Throw "Mock Get-ChildItem called with unexpected Path ($Path)"}
-    Mock Get-Item {Throw "Mock Get-Item called with unexpected Path ($Path)"}
-    Mock Remove-Item {Throw "Mock Remove-Item called with unexpected Path ($Path)"}
-    Mock Test-Path {Throw "Mock Test-Path called with unexpected Path ($Path)"}
+    New-Item 'TestRegistry:\Empty'
+    New-Item 'TestRegistry:\KeyWithChild'
+    New-Item 'TestRegistry:\KeyWithChild\Child'
+    New-Item 'TestRegistry:\KeyWithProperty'
+    Set-ItemProperty 'TestRegistry:\KeyWithProperty' -Name 'SomeProperty' -Value 0
 
-    [string] $fakeRegistryPath = 'HKCU:\Foobar'
+    Mock Remove-Item {}
 
     Context '[Registry key does not exist]' {
-        Mock Test-Path {return $false} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
+        RemoveRegistryKeyIfEmpty 'TestRegistry:\SomeKeyThatDoesNotExist'
 
-        RemoveRegistryKeyIfEmpty $fakeRegistryPath
-
-        It 'Checks if registry key exists' {
-            Assert-MockCalled Test-Path -Times 1 -Exactly
-            Assert-MockCalled Test-Path -Times 1 -Exactly `
-                -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
-                }
+        It 'Does not attempt to remove registry key' {
+            Assert-MockCalled Remove-Item -Times 0
         }
     }
 
     Context '[Empty registry key]' {
-        Mock Get-ChildItem {return @()} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Get-ItemProperty {return @()} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Remove-Item {} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Test-Path {return $true} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        RemoveRegistryKeyIfEmpty $fakeRegistryPath
-
-        It 'Checks if registry key exists' {
-            Assert-MockCalled Test-Path -Times 1 -Exactly
-            Assert-MockCalled Test-Path -Times 1 -Exactly `
-                -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
-                }
-        }
-
-        It 'Checks if registry key has child items' {
-            Assert-MockCalled Get-ChildItem -Times 1 -Exactly
-            Assert-MockCalled Get-ChildItem -Times 1 -Exactly `
-                -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
-                }
-        }
-
-        It 'Checks if registry key has properties' {
-            Assert-MockCalled Get-ItemProperty -Times 1 -Exactly
-            Assert-MockCalled Get-ItemProperty -Times 1 -Exactly `
-                -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
-                }
-        }
+        RemoveRegistryKeyIfEmpty 'TestRegistry:\Empty'
 
         It 'Removes registry key' {
             Assert-MockCalled Remove-Item -Times 1 -Exactly
             Assert-MockCalled Remove-Item -Times 1 -Exactly `
                 -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
+                    $Path -eq 'TestRegistry:\Empty'
                 }
         }
     }
 
     Context '[Registry key with child item]' {
-        $fakeRegistryKey = New-Object -TypeName psobject
-
-        Mock Get-ChildItem {return @($fakeRegistryKey)} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Remove-Item {} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Test-Path {return $true} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        RemoveRegistryKeyIfEmpty $fakeRegistryPath
-
-        It 'Checks if registry key has child items' {
-            Assert-MockCalled Get-ChildItem -Times 1 -Exactly
-            Assert-MockCalled Get-ChildItem -Times 1 -Exactly `
-                -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
-                }
-        }
+        RemoveRegistryKeyIfEmpty 'TestRegistry:\KeyWithChild'
 
         It 'Does not remove registry key' {
             Assert-MockCalled Remove-Item -Times 0
@@ -107,33 +38,7 @@ Describe 'RemoveRegistryKeyIfEmpty Tests' {
     }
 
     Context '[Registry key with property]' {
-        $fakeRegistryKey = New-Object -TypeName psobject
-
-        Mock Get-ChildItem {return @()} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Get-ItemProperty {return @($fakeRegistryKey)} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Remove-Item {} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        Mock Test-Path {return $true} -ParameterFilter {
-            $Path -eq $fakeRegistryPath
-        }
-
-        RemoveRegistryKeyIfEmpty $fakeRegistryPath
-
-        It 'Checks if registry key has child items' {
-            Assert-MockCalled Get-ItemProperty -Times 1 -Exactly
-            Assert-MockCalled Get-ItemProperty -Times 1 -Exactly `
-                -ParameterFilter {
-                    $Path -eq $fakeRegistryPath
-                }
-        }
+        RemoveRegistryKeyIfEmpty 'TestRegistry:\KeyWithProperty'
 
         It 'Does not remove registry key' {
             Assert-MockCalled Remove-Item -Times 0
